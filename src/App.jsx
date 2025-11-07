6b0c979d2b8c74cf6958d3a61b958a9c990bf01c
DianaBosinceanu11 <dianabosinceanu11@gmail.com>
2025-11-07 22:07:22 +0000

    🛠️ Project setup with Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">SkillTrack</h1>
+      <p className="mt-2">Tailwind este activ 🎉</p>
+    </div>
+  );
+}
